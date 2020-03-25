c1f319d1e03cae9cc2aa61d491bfa7f527c354df
ShamefulOdin31 <61662359+ShamefulOdin31@users.noreply.github.com>
2020-03-25 16:37:45 -0500

    Search
    Added search bar to header.php
    Added search page.

--- header.php
+++ header.php
@@ -33,4 +33,10 @@
         <?php endif ?>
     </ul>
 </nav>
+<nav class="navbar navbar-expand-sm navbar-dark">
+    <form class="form-inline my-2 my-lg-0 ml-auto" action="search.php" method="post">
+        <input class="form-control" type="search" placeholder="Search" aria-label="Search" name="search">
+        <button class="btn btn-primary" type="submit">Search</button>
+    </form>
+</nav>
 
