147699a63e8a007d4afa86362696b262135d607d
ShamefulOdin31 <61662359+ShamefulOdin31@users.noreply.github.com>
2020-05-19 15:25:02 -0500

    Update search.php
    Changed page layour

--- search.php
+++ search.php
@@ -1,27 +1,70 @@
 <?php 
     require "connect.php";
-
-	
-
     session_start();
 
-    $results;
-    
-    $search = filter_input(INPUT_POST, 'search', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
-    $search = "%" . $search . "%";
-
-
-    $query = "SELECT * FROM dndcharacters WHERE searchBy LIKE :search";
-
-    if($statement = $db->prepare($query))
+    if($_SERVER["REQUEST_METHOD"] == "POST")
     {
-        $statement->bindParam(":search", $search);
+        $type = filter_input(INPUT_POST, "type", FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+        $search = filter_input(INPUT_POST, "search", FILTER_SANITIZE_FULL_SPECIAL_CHARS);
 
-        if($statement->execute())
+        $_SESSION['searchType'] = $type;
+        $_SESSION['search'] = $search;
+
+        $catResults;
+        $characterResults;
+        $spellResults;
+
+        $searchType = $_SESSION['searchType'];
+        $search = $_SESSION['search'];
+        //$search = "%" . $search . "%";
+        if($searchType == "catname")
         {
-            $results = $statement->fetchAll();
+            $query = "SELECT * FROM categories where catname LIKE :catname";
+
+            if($statement = $db->prepare($query))
+            {
+                
+                $statement->bindParam(":catname", $search);
+
+                if($statement->execute())
+                {
+                    $catResults = $statement->fetchAll();
+                }
+            } 
+        }
+
+        elseif($searchType == "cname")
+        {
+            
+            $query = "SELECT * FROM dndcharacters where cname LIKE :cname";
+
+            if($statement = $db->prepare($query))
+            {
+                $statement->bindParam(":cname", $search);
+
+                if($statement->execute())
+                {
+                    $characterResults = $statement->fetchAll();
+                }
+            }
+        }
+        
+        elseif($searchType == "spellName")
+        {
+            $spellsJSON = file_get_contents("spells.json");
+            $spellsOutput = json_decode($spellsJSON, true);
+
+            foreach($spellsOutput as $key => $value)
+            {
+                if($value['name'] == $search)
+                {
+                    $spellResults = array();
+                    array_push($spellResults, $value);
+                }
+            }
         }
     }
+    
 ?>
 
 <!DOCTYPE html>
@@ -41,30 +84,72 @@
 <?php require "header.php"?>
 <?php require "searchBar.php"?>
 <div class="container">
-    <table class="table table-striped table-hover">
-        <thead class="thead-dark">
-            <tr>
-                <th scope="col"><a href="index.php?id=1&type=name">Name</a></th>
-                <th scope="col"><a href="index.php?id=2&type=race">Race</a></th>
-                <th scope="col"><a href="index.php?id=3&type=class">Class</a></th>
-                <th scope="col"><a href="index.php?id=4&type=background">Background</a></th>
-                <th scope="col">Notes</th>
-                <th scope="col"></th>
-            </tr>
-        </thead>
-        <tbody>
-            <?php foreach($results as $key => $value) :?>
+    <?php if(isset($catResults)):?>
+        <table class="table table-striped table-hover">
+            <thead class="thead-dark">
                 <tr>
-                    <th scope="row"><?= $value["cname"] ?></th>
-                    <td><?= $value['race'] ?></td>
-                    <td><?= $value['class'] ?></td>
-                    <td><?= $value['background'] ?></td>
-                    <td><?= $value['notes'] ?></td>
-                    <td><a href="select.php?characterID=<?= $value['characterID'] ?>&type=<?= str_replace(' ', '-', $value['searchBy']) ?>">Select</a></td>
+                    <th scope="col">Name</th>
+                    <th scope="col"></th>
                 </tr>
-            <?php endforeach ?>
-        </tbody>
-    </table>
+            </thead>
+            <tbody>
+                <?php foreach($catResults as $key => $value) :?>
+                    
+                    <tr>
+                        <th scope="row"><?= $value['catname'] ?></th>
+                        <td><a href="categories.php?id=<?= $value['categoryID'] ?>&name=<?= $value['catname'] ?>">Select</a></td>
+                    </tr>
+                <?php endforeach?>
+            </tbody>
+        </table>
+    <?php endif?>
+    <?php if(isset($characterResults)):?>
+        <table class="table table-striped table-hover">
+            <thead class="thead-dark">
+                <tr>
+                    <th scope="col">Name</th>
+                    <th scope="col">Race</th>
+                    <th scope="col">Class</th>
+                    <th scope="col">Background</th>
+                    <th scope="col">Notes</th>
+                    <th scope="col"></th>
+                </tr>
+            </thead>
+            <tbody>
+                    <?php foreach($characterResults as $key => $value) :?>
+                        <tr>
+                            <th scope="row"><?= $value['cname'] ?></th>
+                            <td><?= $value['race'] ?></td>
+                            <td><?= $value['class'] ?></td>
+                            <td><?= $value['background'] ?></td>
+                            <td><?= $value['notes'] ?></td>
+                            <td><a href="select.php?characterID=<?= $value['characterID'] ?>&type=<?= str_replace(' ', '-', $value['searchBy']) ?>">Select</a></td>
+                        </tr>
+                    <?php endforeach?>
+            </tbody>
+        </table>
+    <?php endif?>
+    <?php if(isset($spellResults)):?>
+        <table class="table table-striped table-hover">
+            <thead class="thead-dark">
+                <tr>
+                    <th scope="col">Name</th>
+                    <th scope="col">Race</th>
+                    <th scope="col">Class</th>
+                    <th scope="col">Background</th>
+                    <th scope="col">Notes</th>
+                    <th scope="col"></th>
+                </tr>
+            </thead>
+            <tbody>
+                    <?php foreach($spellsOutput as $key => $value) :?>
+                        <tr>
+                            <th scope="row"><?= $value['name'] ?></th>
+                        </tr>
+                    <?php endforeach?>
+            </tbody>
+        </table>
+    <?php endif?>
 </div>
 </body>
 </html>
