bbd5808874b0a591b1274b4e687240f31c318844
ShamefulOdin31 <61662359+ShamefulOdin31@users.noreply.github.com>
2020-05-22 18:58:20 -0500

    additional clearnup

--- connect.php
+++ connect.php
@@ -1,11 +1,15 @@
 <?php 
     define('DB_DSN', 'mysql:host=localhost;dbname=dndproject;charset=utf8');
-    define('DB_USER', 'serveruser2');
-    define('DB_PASS','richard');
+    define('DB_USER', 'richard');
+    define('DB_PASS','password');
 
     // login for first phpmyadmin login screen it
     // Username: serveruser2
     // Password: password
+
+    // Login for remote phpmyadmin 
+    // Username : root
+    // Password: richard
     try
     {
         $db = new PDO(DB_DSN, DB_USER, DB_PASS);
