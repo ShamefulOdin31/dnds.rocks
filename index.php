418495d5bbc5c2d74b059916c70217f6a64f0391
ShamefulOdin31 <61662359+ShamefulOdin31@users.noreply.github.com>
2020-05-22 16:24:10 -0500

    Removed table

--- index.php
+++ index.php
@@ -1,59 +1,6 @@
 <?php 
     require "connect.php";
     session_start();
-
-    
-
-    $queryResults;
-
-    $query = "SELECT cname, race, class, background, notes, userOwner, strength, intelligence, dexterity, wisdom, constitution, charisma, hitpoints, searchBy, username, characterID 
-                FROM dndcharacters 
-                JOIN logins  
-                ON dndcharacters.userOwner = logins.loginID 
-                WHERE visibility = 'y'";
-
-    $sort = ["name" => " ORDER BY cname",
-    "race" => " ORDER BY race",
-    "class" => " ORDER BY class",
-    "background" => " ORDER BY background",
-    "owner" => "ORDER BY userOwner"];
-
-    if(isset($_GET['id']) && isset($_GET['type']))
-    {
-        if($_GET['id'] == '1' && $_GET['type'] == 'name')
-        {
-            $query .= $sort["name"];
-        }
-
-        elseif($_GET['id'] == '2' && $_GET['type'] == 'race')
-        {
-            $query .= $sort["race"];
-        }
-
-        elseif($_GET['id'] == '3' && $_GET['type'] == 'class')
-        {
-            $query .= $sort["class"];
-        }
-
-        elseif($_GET['id'] == '4' && $_GET['type'] == 'background')
-        {
-            $query .= $sort["background"];
-        }
-        elseif($_GET['id'] == '5' && $_GET['type'] == 'owner')
-        {
-            $query .= $sort["owner"];
-        }
-
-        else
-        {
-            http_response_code(404);
-            die();
-        }
-    }
-
-    $statement = $db->prepare($query);
-    $statement->execute();
-    $queryResults = $statement->fetchAll();
 ?>
 
 <!DOCTYPE html>
@@ -72,34 +19,13 @@
 <body>
 <?php require "header.php"?>
 <!-- Start of content -->
-<div class="container">
-    <table class="table table-striped table-hover">
-        <thead class="thead-dark">
-            <tr>
-                <th scope="col"><a href="index.php?id=1&type=name">Name</a></th>
-                <th scope="col"><a href="index.php?id=2&type=race">Race</a></th>
-                <th scope="col"><a href="index.php?id=3&type=class">Class</a></th>
-                <th scope="col"><a href="index.php?id=4&type=background">Background</a></th>
-                <th scope="col">Notes</th>
-                <th scope="col"><a href="index.php?id=5&type=owner">Owner</a></th>
-                <th scope="col"></th>
-            </tr>
-        </thead>
-        <tbody>
-            <?php foreach($queryResults as $key => $value) :?>
-                <tr>
-                    <th scope="row"><?= $value["cname"] ?></th>
-                    <td><?= $value['race'] ?></td>
-                    <td><?= $value['class'] ?></td>
-                    <td><?= $value['background'] ?></td>
-                    <td><?= $value['notes'] ?></td>
-                    <td><?= $value['username'] ?></td>
-                    <td><a href="select.php?characterID=<?= $value['characterID'] ?>&type=<?= str_replace(' ', '-', $value['searchBy']) ?>">Select</a></td>
-                </tr>
-            <?php endforeach ?>
-        </tbody>
-    </table>
-</div>
+    <div class="container">
+        <?php if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] == true):?>
+            <p>You must go to the account page in order to use this site</p>
+            <button type="button" class="btn bnt-primary btn-lg"><a href="account.php"></a>Account</button>
+        <?php else: ?>
 
+        <?php endif ?>
+    </div>
 </body>
 </html>
